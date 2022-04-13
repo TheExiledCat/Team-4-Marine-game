13fb32f382bdce23641ab22fab63d6610c06f0e5
armando-roerdinkveldboom-lw <kazekage2002@hotmail.com>
2022-04-13 14:20:16 +0200

    feat(New Station): added new modle for generator, WIP

--- Team-4-Marine/Assets/Scripts/Node System/RepairStations/Station Components/Indicator.cs
+++ Team-4-Marine/Assets/Scripts/Node System/RepairStations/Station Components/Indicator.cs
@@ -6,25 +6,22 @@ public class Indicator : MonoBehaviour
 {
     private bool m_LitUp;
     private Color m_OriginalColor;
+
     private void Start()
     {
     }
+
     public void Initiate()
     {
         m_OriginalColor = GetComponent<MeshRenderer>().material.GetColor("_EmissionColor");
     }
-    private void Update()
-    {
-        if (GameManager.GM.m_EngineerControls.Interactions.PrimaryInteract.WasPressedThisFrame())
-        {
-            ToggleIndicator();
-        }
-    }
+
     public void SetIndicator(bool _status)
     {
         m_LitUp = _status;
         GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", m_LitUp ? m_OriginalColor : Color.black);
     }
+
     public void ToggleIndicator()
     {
         m_LitUp = !m_LitUp;
