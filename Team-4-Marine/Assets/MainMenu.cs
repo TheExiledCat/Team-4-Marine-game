08898901d3c6ffd66ad2e538be9f66cd1343c755
BOOSTEDmati <25820@ma-web.nl>
2022-05-26 12:33:49 +0200

    feat(MainMenu) added TMP and changed the layout of the buttons

--- Team-4-Marine/Assets/MainMenu.cs
+++ Team-4-Marine/Assets/MainMenu.cs
@@ -7,8 +7,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    private Button m_Start;
-    private Image m_image;
     // Start is called before the first frame update
     void Start()
     {
